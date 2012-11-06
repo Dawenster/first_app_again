83ee76df9befb309f0aec4a94156de24fd0ecee6
David Wen <dstwen@gmail.com>
2012-11-05 23:00:14 -0800

    Revert "Revert "Revert "Revert "Revert "Initial commit"""""
    This reverts commit b3b263d4278523608e77feca7bd5f0454e6a9545.

--- first_app_again/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstAppAgain::Application.config.secret_token = '5341b78b6fea34e39d1024acffb372cf3fa7d82f10b5a0c35cd1a6dc0f098dd148e9ba758e66dec8f5f7583dc3dc26fb7376b634ea80ab6a1db5c5dbb3153b8c'
